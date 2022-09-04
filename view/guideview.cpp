ec5c0e79775c9a531124ba5f0634fd88395a836e
Rino Sincic <rino.sincic0@gmail.com>
2022-09-04 17:15:34 +0200

    guideview

--- view/guideview.cpp
+++ view/guideview.cpp
@@ -2,21 +2,49 @@
 
 GuideView::GuideView(const QSize& s,View* parent): View(s,parent)
 {
-    QHBoxLayout* mainLayout = new QHBoxLayout;
-    mainLayout->setSpacing(10);
+    QVBoxLayout* mainLayout = new QVBoxLayout;
     mainLayout->setMargin(0);
     mainLayout->setContentsMargins(150,50,150,50);
     mainLayout->setAlignment(Qt::AlignCenter);
+    mainLayout->addLayout(createDescription(tr(
+    "Filament3dPrint è un programma che ti può aiutare a tenere\n"
+    "traccia delle stampe fatte con una Stampante 3D.\n"
+    "Quando si fa una stampa 3D è utile tenere traccia di:\n"
+    "- Materiale\n"
+    "- Durata\n"
+    "- Consumo\n"
+    "- Data\n"
+    "\n"
+    "Il materiale è organizzato in bobine vendute al Kg quindi\n"
+    "è utili tenere traccia per ogni stampa quale e quanti grammi\n"
+    "di materiale si sono usati.\n"
+    "è importante tenere traccia della durata in quanto\n"
+    "una stampa 3D può durare svariate ore.\n"
+    "\n"
+    "Questo programma infine offre dei utili grafici\n"
+    "con cui esaminare queste informazioni"),
+    ":/3dPrint.jpeg"));
 
-    nuovo = new QPushButton("Nuovo",this);
-    carica = new QPushButton("Apri esistente",this);
-    nuovo->setFixedSize(150,70);
-    carica->setFixedSize(150,70);
-
-    mainLayout->addWidget(nuovo);
-    mainLayout->addWidget(carica);
-
-    resize(QSize(800, 600));
 
     setLayout(mainLayout);
+}
+
+QLayout* GuideView::createDescription(const QString& description, const QString& imgPath){
+    //Parte destra immagine e descrizione
+    QVBoxLayout* descrLayout = new QVBoxLayout;
+    //Immagine stampante descrizione
+    QLabel* img = new QLabel(this);
+    QPixmap pic = QPixmap(imgPath);
+    pic = pic.scaledToHeight(300);
+    img->setPixmap(pic);
+    img->setFixedSize(300,300);
+    descrLayout->addWidget(img,Qt::AlignHCenter);
+
+    //Descrizione
+    QLabel* desc = new QLabel(description,this);
+    QFont f("Calibri", 9);
+    desc->setFont(f);
+    descrLayout->addWidget(desc,Qt::AlignJustify);
+
+    return descrLayout;
 }
--- view/guideview.h
+++ view/guideview.h
@@ -13,11 +13,43 @@ class GuideView: public View
 {
     Q_OBJECT
 private:
-    QPushButton* nuovo;
-    QPushButton* carica;
 
+    /**
+     * @brief createDescriptionLayout
+     * Metodo che si occupa di realizzare e predisporre il layout con la parte descrittiva delLa Home
+     * @param description descrizione da inserire all'interno del layout
+     * @param imgPath path della immagine da includere nel layout
+     * @return layout contenente la parte della descrizione della applicazione
+     * private perchè invocato solamente nel costruttore
+     */
+    QLayout* createDescription(const QString& description, const QString& imgPath);
+    /**
+     * @brief connectViewSignals Metodo virtuale
+     * Questo Metodo serve a connettere i SIGNAL degli elementi grafici della Homeview ai SIGNAL della
+     * HomeView i quali dovranno comunicare con il Controller HomeCtrl
+     */
+
+    //void connectViewSignals() const override;
+
+protected:
+    /**
+     * @brief closeEvent Overriding del metodo closeEvent implementato da QWidget
+     * Tramite questo Overriding si cerca di modificare il da farsi nel evento di chiusura della window con la X.
+     * In particolare questo metodo chiede una conferma al utente prima di chiudere.
+     * Viene poi emesso un segnale per avvisare il controller della chiusura
+     * @param event Evento di chiusura della scermata window
+     */
+    //void closeEvent(QCloseEvent *event) override;
 public:
+    /**
+     * @brief GuideView
+     * Il Costruttore si occupa di Creare la View e creare i due layout laterali della app e mostrarli.
+     * @param s Grandezza della finistra, la windowSize
+     */
     explicit GuideView(const QSize& s = QSize(),View* parent = nullptr);
+
+
+public slots:
 
 };
 
